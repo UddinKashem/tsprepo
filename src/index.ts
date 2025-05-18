f2913dc5825c5de55c4e093c22f466e678ec0000
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-17 22:29:23 -0400

    Add Objects

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,24 @@
 // const enum Size {Small ='s', Medium='m', Large='l'};
 // let mysize: Size = Size.Medium;
 // console.log(mysize);
-//Function:-----------------
-function calculateTax(income: number, taxYear: number): number{
-    if(taxYear < 50_000)
-      return income * 1.2;
-    return income * 1.3;
-}
-
-console.log(calculateTax(100_000, 2024));
+// //Function:-----------------
+// function calculateTax(income: number, taxYear: number): number{
+//     if(taxYear < 50_000)
+//       return income * 1.2;
+//     return income * 1.3;
+// }
+// console.log(calculateTax(100_000, 2024));
+//----------------------------------
+//Objects:
+let employee: {
+    id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1,
+    name: 'Saife',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+};
+console.log(employee);
