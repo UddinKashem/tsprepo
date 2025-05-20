2b6e7130e830ac67736827c19484337ea173675c
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-20 13:13:10 -0400

    Update TSconfig.json for Decorators

--- src/classObject_ReadOnly_Options.ts
+++ src/classObject_ReadOnly_Options.ts
@@ -452,4 +452,28 @@ operations. Just define the class with the right members. */
 // type Nullable<T> ={
 //     [K in keyof T]: T[K] | null;
 // }
-
+//=======================================
+//Generic Exercise:
+//====================================
+//Convert the function below to a generic function:
+// function echo<T>(arg: T): T { return arg; }
+// /*When compiling the following piece of code, we get an error saying ‘Property name does not exist on type T’.
+// * How can we solve this problem?
+// */
+// function printName<T extends {name: string}>(obj: T) {
+// console.log(obj.name);
+// }
+// /**
+//  * An Entity should have a unique identifier. The type of identifier, however, is dependenton the use case. In some cases, 
+//  * the ID might be a number, in other cases, it might be a string, GUID, etc. Represent the entity using a generic class.
+//  */
+// class Entity<T>{
+//     constructor (public id: T){}
+// }
+// /**
+//  * Given the following interface, what does keyof User return?
+//  */
+// interface User {
+// userId: number;
+// username: string;
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-interface Product {
-    name: string;
-    price: number;
+//Convert the function below to a generic function:
+function echo<T>(arg: T): T { return arg; }
+/*When compiling the following piece of code, we get an error saying ‘Property name does not exist on type T’.
+* How can we solve this problem?
+*/
+function printName<T extends {name: string}>(obj: T) {
+console.log(obj.name);
 }
-
-type ReadOnly<T> = {
-    readonly [ K in keyof T]: T[K];
-}
-
-type Optional<T> = {
-    [K in keyof T]?: T[K];
-}
-
-type Nullable<T> ={
-    [K in keyof T]: T[K] | null;
-}
+/**
+ * An Entity should have a unique identifier. The type of identifier, however, is dependenton the use case. In some cases, 
+ * the ID might be a number, in other cases, it might be a string, GUID, etc. Represent the entity using a generic class.
+ */
