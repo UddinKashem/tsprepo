9247832efa9415d7ed2068c551ce9a2db9608a6e
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-20 15:53:36 -0400

    Update Decorator

--- src/classObject_ReadOnly_Options.ts
+++ src/classObject_ReadOnly_Options.ts
@@ -494,4 +494,21 @@ operations. Just define the class with the right members. */
 // }
 //====================================
 //Decorator Factory:
-//===========================
+// //===========================
+// type ComponentOptions = {
+//     selector: string
+// }
+// function Component(options: ComponentOptions){
+//     return (constructor : Function) => {
+//         console.log('Component decorator Called');
+//         constructor.prototype.options = options;
+//         constructor.prototype.uniqueId = Date.now();
+//         constructor.prototype.insertInDOM = () => {
+//         console.log('Inserting the component in the DOM.');
+//         }
+//     }
+// }
+
+// @Component({ selector: '#my-profile'})
+// class ProfileComponent{
+// ]
