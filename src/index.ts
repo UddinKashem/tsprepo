51bf5afa3035d61f497bb44aa1b90a67c593c7b7
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-21 14:55:48 -0400

    Update Re-exporting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { Circle, Square } from "./shape";
+//import { Circle, Square } from "./shape";
+/**Default Export */
 //import Store, { Format } from "./storage";
+/**Wildcard Import */
+//import * as Shape from "./shapes"
 
-let circle = new Circle(56);
-console.log("Area of Circle: " + circle.radious);
+// let circle = new Circle.Circle(56);
+// console.log("Area of Circle: " + circle.radious);
 
-let square = new Square(54);
-console.log(square);
+// let square = new Square.Square(54);
+// console.log(square);
 
 // let store = new Store();
 // console.log(store);
+import { Circle, Square } from "./shapes";
+
+let circle = new Circle(5);
+console.log("Area of Circle: " + circle.radious);
+
+let square = new Square(34);
+console.log("Area of Square: " + square.width);
+
