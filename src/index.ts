b862f3f737916c0ee51b640b4cf250f3b40c27cc
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-21 15:27:27 -0400

    Integrate with JavaScript

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-//import { Circle, Square } from "./shape";
-/**Default Export */
-//import Store, { Format } from "./storage";
-/**Wildcard Import */
-//import * as Shape from "./shapes"
+import { calculateTax } from "./tax";
 
-// let circle = new Circle.Circle(56);
-// console.log("Area of Circle: " + circle.radious);
-
-// let square = new Square.Square(54);
-// console.log(square);
-
-// let store = new Store();
-// console.log(store);
-import { Circle, Square } from "./shapes";
-
-let circle = new Circle(5);
-console.log("Area of Circle: " + circle.radious);
-
-let square = new Square(34);
-console.log("Area of Square: " + square.width);
-
+let tax = calculateTax(34500);
+console.log(tax);
