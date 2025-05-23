75564d099c843a912329e64e0b9a645265b9381b
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-23 12:52:12 -0400

    Adding component on the react

--- reminder-app/src/components/newReminder.tsx
+++ reminder-app/src/components/newReminder.tsx
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
-import { JSX } from 'react/jsx-runtime';
+import React, { JSX, useState } from 'react';
 
-function newReminder(): JSX.Element {
-    //const [title, setTitle] = useState('');
+function NewReminder(): JSX.Element {
     const [title, setTitle] = useState('');
 
-    const submitForm = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log(title);
-    }
-
     return (
-        <form onSubmit={submitForm}>
-           <label htmlFor="title"></label>
-           <input value={title} onChange={e => setTitle(e.target.value)} id="title" type="text" className="form-control" />
-           <button type="submit" className="btn btn-primary my-3 rounded-pill">Add Reminder</button>    
+        <form>
+            <label htmlFor="title"></label>
+            <input value={title} onChange={e => setTitle(e.target.value)} id="title" type="text" className="form-control" />
+            <button type="submit" className="btn btn-primary my-3 rounded-pill">Add Reminder</button>
         </form>
     );
 }
 
-export default newReminder;
+export default NewReminder;
